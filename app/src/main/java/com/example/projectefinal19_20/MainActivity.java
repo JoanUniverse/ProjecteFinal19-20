1181534ace3233b2d89903fc538b080d644c3922
JoanUniverse <jonnyy184@gmail.com>
2020-05-15 11:52:15 +0200

    Primer listView orientació

--- app/src/main/java/com/example/projectefinal19_20/MainActivity.java
+++ app/src/main/java/com/example/projectefinal19_20/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.projectefinal19_20;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Primer tema");
+        arrayList.add("Segon tema");
+        arrayList.add("Tercer tema");
+        arrayList.add("Primer tema");
+        arrayList.add("Segon tema");
+        arrayList.add("Tercer tema");
+        arrayList.add("Primer tema");
+        arrayList.add("Segon tema");
+        arrayList.add("Tercer tema");
+        arrayList.add("Primer tema");
+        arrayList.add("Segon tema");
+        arrayList.add("Tercer tema");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
     }
 }
