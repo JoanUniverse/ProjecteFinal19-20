32d8310e8c5550ee7190629ad78b24a0a26b0c9c
JoanUniverse <jonnyy184@gmail.com>
2020-05-26 19:35:51 +0200

    Intent de Login Activity cap a Main Activity

--- app/src/main/java/com/example/projectefinal19_20/LoginActivity.java
+++ app/src/main/java/com/example/projectefinal19_20/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.projectefinal19_20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onEnviarButtonClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
