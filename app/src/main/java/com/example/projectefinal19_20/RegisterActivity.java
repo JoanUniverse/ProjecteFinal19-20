fd16cba536aa7e286b824bd326209dddabd0e74a
JoanUniverse <jonnyy184@gmail.com>
2020-06-19 13:55:08 +0200

    Nova classe Async per poder fer el POST Register amb dades de prova en local

--- app/src/main/java/com/example/projectefinal19_20/RegisterActivity.java
+++ app/src/main/java/com/example/projectefinal19_20/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.example.projectefinal19_20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public class RequestAsync extends AsyncTask<String,String,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                // POST Request
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("Email", "b@b.b");
+                postDataParams.put("Password", "Hola1234.");
+                postDataParams.put("ConfirmPassword", "Hola1234.");
+
+                return RequestHandler.sendPost("https://localhost:44375/api/Account/Register",postDataParams);
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null){
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
